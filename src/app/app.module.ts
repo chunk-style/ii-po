e772f04c0dc1119251ab80d4daa6fdc39a021873
chunk-style <github@ethanrepp.com>
2023-05-05 18:38:24 -0700

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { CovalentLayoutModule } from "@covalent/core/layout";
+
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, CovalentLayoutModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
